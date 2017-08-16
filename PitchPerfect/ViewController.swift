26e47f3448cc8d1bd8260606fe3ff96f25ea032f
kevin578 <kbriggs@2600-6c64-607f-dc68-0c5b-c423-3944-ed29.dhcp6.chtrptr.net>
2017-08-16 10:14:00 -0400

    record and stop buttons work

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -12,14 +12,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        recordingLabel.text = "waiting"
+        stop.isEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func record(_ sender: Any) {
+        print("button was pressed")
+        recordingLabel.text = "recording"
+        stop.isEnabled = true
+        record.isEnabled = false
+    }
+    
+    @IBAction func stopButton(_ sender: Any) {
+        record.isEnabled = true
+        stop.isEnabled = false
+        recordingLabel.text = "Waiting to record"
+    }
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    @IBOutlet weak var stop: UIButton!
 
+    @IBOutlet weak var record: UIButton!
 
 }
 
